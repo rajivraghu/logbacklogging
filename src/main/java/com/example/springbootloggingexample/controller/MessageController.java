f97b79300102660ebc31208772b3740fb351ccfc
rajivraghu <rajiv.vedavyas@gmail.com>
2022-07-21 14:50:28 +0000

    json logs

--- src/main/java/com/example/springbootloggingexample/controller/MessageController.java
+++ src/main/java/com/example/springbootloggingexample/controller/MessageController.java
@@ -13,10 +13,19 @@ public class MessageController {
 	@GetMapping("/message")
 	public String getMessage() {
 		logger.info("[getMessage] info message");
-		logger.warn("[getMessage] warn message");
-		logger.error("[getMessage] error message");
-		logger.debug("[getMessage] debug message");
-		logger.trace("[getMessage] trace message");
+		// logger.warn("[getMessage] warn message");
+		// logger.error("[getMessage] error message");
+		// logger.debug("[getMessage] debug message");
+		// logger.trace("[getMessage] trace message");
+
+		String s = null;
+		try{
+			s.length();
+		}
+		catch(Exception ex){
+
+			logger.error("err",ex);
+		}
 		//System.out.println(10/0);
 		return "open console to check log messages.";
 	}
